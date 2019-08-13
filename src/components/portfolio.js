7742276e9616326f7a191ce0d3efba42a459c2c0
github username <kahyap13@gmail.com>
2019-08-13 18:34:44 -0400

    add background

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import Gallery from './gallery'
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const Portfolio = () => {
-  return(
-    <div className="portfolio__container">
-      <span className="text1"> Kah's </span>
-      <span className="text2"> Portfolio </span>
-    </div>
-  )
+class Portfolio extends Component {
+  render() {
+    return(
+      <div className="portfolio__container">
+            <span className="text1"> Kah's </span>
+            <span className="text2"> Portfolio </span>
+      </div>
+    )
+  }
 }
 
 export default Portfolio;
